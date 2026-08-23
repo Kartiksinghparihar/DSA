016a91f186f3a0cc18f600da5b371faa3371d19d
Kartik Singh Parihar <kartiksinghp2006@gmail.com>
2026-08-23 17:43:43 +0530

    Time: 128 ms (5.09%) | Memory: 14.1 MB (86.77%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,25 +1,17 @@
-#include <vector>
-#include <unordered_map>
-
 class Solution {
 public:
-    std::vector<int> twoSum(std::vector<int>& nums, int target) {
-        // Map to store the value of an element and its corresponding index
-        std::unordered_map<int, int> seen_numbers; 
-        
-        for (int i = 0; i < nums.size(); ++i) {
-            int complement = target - nums[i];
-            
-            // Check if the required number to reach the target has already been seen
-            if (seen_numbers.find(complement) != seen_numbers.end()) {
-                return {seen_numbers[complement], i};
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]+nums[j]==target){
+                    ans.push_back(i);
+                    ans.push_back(j);
+                }
             }
-            
-            // If not found, track the current number and its index
-            seen_numbers[nums[i]] = i;
         }
+        return ans;
         
-        // Return an empty vector if no solution is found (guaranteed not to hit based on constraints)
-        return {};
     }
 };
